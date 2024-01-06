fcd44151592c6019a8dad5c7b8ef1d3a03315b0d
Hoang Lai <nguyenlai21617@gmail.com>
2024-01-06 19:40:18 +0700

    Modify signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
-    root 'account_form#user_login'
+    root 'account_form#user_signup'
     get 'mypage', to: 'mypage#show'
 
   end
