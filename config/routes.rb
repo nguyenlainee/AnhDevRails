7d0aa8a6d6e039e8efcb153a2bcb127f54236293
Hoang Lai <nguyenlai21617@gmail.com>
2024-01-08 21:22:54 +0700

    Add locales (vi, en)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
     root 'mypage#show'
     get 'mypage', to: 'mypage#show'
+    get 'mypage/contact', to: 'mypage#contact'
   end
 end
