925b705f513ba517f86c87fa859637f58fd7f74f
Hoang Lai <nguyenlai21617@gmail.com>
2024-01-09 22:14:39 +0700

    Add user_profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
     root 'mypage#show'
+
     get 'mypage', to: 'mypage#show'
     get 'mypage/contact', to: 'mypage#contact'
     get 'mypage/about_me'
+    get 'mypage/user_profile'
+    get 'mypage/billing'
+    get 'mypage/security'
+    get 'mypage/notification'
   end
 end
